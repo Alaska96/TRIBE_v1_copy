f9466108dfec336696f578affca6cccaf8f7ed79
Alchaima Sikebir <130755445+Alaska96@users.noreply.github.com>
2026-06-03 09:32:49 +0200

    Update run_ensemble.py

--- algonauts2025/grids/run_ensemble.py
+++ algonauts2025/grids/run_ensemble.py
@@ -9,17 +9,17 @@ from modeling_utils.utils import run_grid
 from ..main import Experiment  # type: ignore
 from .defaults import PROJECT_NAME, SAVEDIR, default_config
 
-GRID_NAME = "model_soup"
+GRID_NAME = "baseline_ensemble"
 
 update = {
     "infra": {
         "cluster": "auto",
         "folder": SAVEDIR,
-        "slurm_partition": "partition",
+        "slurm_partition": "ulow",
         "job_name": PROJECT_NAME,
     },
     "wandb_config.group": GRID_NAME,
-    "save_checkpoints": False,
+    "save_checkpoints": True,
     "seed": None,
     "patience": None,
 }
@@ -46,7 +46,7 @@ if __name__ == "__main__":
         grid,
         job_name_keys=["wandb_config.name", "infra.job_name"],
         combinatorial=True,
-        n_randomly_sampled=1000,
+        n_randomly_sampled=5,
         overwrite=False,
         dry_run=False,
         infra_mode="force",
